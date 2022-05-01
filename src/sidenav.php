f8db1a524787392bbf9a7e31ac9bdd691aef352b
ghpvampiro <gustavo_hp@hotmail.com>
2022-05-01 15:23:31 -0300

    Update sidenav.php

--- src/sidenav.php
+++ src/sidenav.php
@@ -5,9 +5,9 @@ $sidenav =
 '<div class="sidenav">
 <a href="#">Página Inicial</a>
 <a href="#">API</a>
-<a href="./src/connect.php">Teste de Conexão</a>
+<a href="https://github.com/gushpat">Acessar no GitHub</a>
 
-<b><img src="https://s2.glbimg.com/paF5KTEVGzMU-ZcZa2mjYicNDjM=/e.glbimg.com/og/ed/f/original/2015/03/09/ada.jpg" alt="Ada Lovelace" width="100" height="100"><p>Ada v.1a</p></b>
+<b><img src="https://s2.glbimg.com/paF5KTEVGzMU-ZcZa2mjYicNDjM=/e.glbimg.com/og/ed/f/original/2015/03/09/ada.jpg" alt="Ada Lovelace" width="100" ><p>Ada v.1a</p></b>
 
 </div>';
 
