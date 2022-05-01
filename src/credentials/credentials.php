948150485566a72c39717ff4c3c473000576832a
ghpvampiro <gustavo_hp@hotmail.com>
2022-05-01 13:05:31 -0300

    Create credentials.php

---
+++ src/credentials/credentials.php
@@ -0,0 +1,16 @@
+<?php
+
+//ESTE ARQUIVO POSSUI TODAS AS CREDENCIAIS QUE SERÃO UTILIZADAS PELO SISTEMA
+//Credenciais de acesso ao banco de dados
+//para acessar basta alterar o valor de $host, $user, $pass e $db
+
+
+$servername = "localhost";
+$username = "root"; 
+$password = ""; 
+$dbname = "ada_api";
+
+
+
+
+?>
