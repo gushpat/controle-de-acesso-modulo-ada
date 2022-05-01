bda758a9e6708a3e18dc219784f75423804290f5
ghpvampiro <gustavo_hp@hotmail.com>
2022-05-01 18:45:08 -0300

    Update connect.php

--- src/connect.php
+++ src/connect.php
@@ -1,3 +1,10 @@
+<!-- 
+  Autor: Gustavo Patricio
+  Data de criação: 01/05/2022
+  Data de modificação: 01/05/2022
+  Versão: 0.01
+-->
+
 <?php
 
 //importa credenciais de acesso ao banco de dados
