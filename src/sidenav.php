82f11c4cc1f8e09cacd8364713978eb44b687afd
ghpvampiro <gustavo_hp@hotmail.com>
2022-05-01 13:04:55 -0300

    Create sidenav.php

---
+++ src/sidenav.php
@@ -0,0 +1,15 @@
+<?php
+
+$sidenav = 
+
+'<div class="sidenav">
+<a href="#">Página Inicial</a>
+<a href="#">API</a>
+<a href="./src/connect.php">Teste de Conexão</a>
+
+<b><img src="https://s2.glbimg.com/paF5KTEVGzMU-ZcZa2mjYicNDjM=/e.glbimg.com/og/ed/f/original/2015/03/09/ada.jpg" alt="Ada Lovelace" width="100" height="100"><p>Ada v.1a</p></b>
+
+</div>';
+
+
+?>
