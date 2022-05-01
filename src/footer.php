82f03c444df22cefa3140f82f4c2dd7eb05291a3
ghpvampiro <gustavo_hp@hotmail.com>
2022-05-01 18:45:12 -0300

    Create footer.php

---
+++ src/footer.php
@@ -0,0 +1,18 @@
+
+<?php
+
+  
+$footer = 
+"
+
+
+<div class='footer'>
+<p href='github.com/gushpat' style='font-weight: bold; font-size: 17px;' >Desenvolvido por Gustavo Patricio</p>
+<p style='font-weight: bold; font-size: 17px;'>Made with ❤️ in Sorocaba-SP || 2022 </p>
+</div>
+
+
+";
+
+
+?>
