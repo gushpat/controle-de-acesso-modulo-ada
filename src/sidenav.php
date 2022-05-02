02d13bac635966b451b0118ce51e0a107a1c20b2
ghpvampiro <gustavo_hp@hotmail.com>
2022-05-02 15:09:53 -0300

    Update sidenav.php

--- src/sidenav.php
+++ src/sidenav.php
@@ -6,8 +6,8 @@ $sidenav =
 
 '<div class="sidenav">
 <a href="#">Página Inicial</a>
-<a href="#">API</a>
-<a href="https://github.com/gushpat">Acessar no GitHub</a>
+<a href="/api/v1/api.php?token="204863"&usuarioid=1&dataacesso="2022-05-04 22:21:32"&acao="1"">API</a>
+<a href="https://github.com/gushpat/controle-de-acesso-modulo-ada">Acessar no GitHub</a>
 
 <b><img src="https://s2.glbimg.com/paF5KTEVGzMU-ZcZa2mjYicNDjM=/e.glbimg.com/og/ed/f/original/2015/03/09/ada.jpg" alt="Ada Lovelace" width="100" ><p>Ada v.1a</p></b>
 
