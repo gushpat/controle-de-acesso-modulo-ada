0b87c328cc0eabc7c1a6d6018c4a129cde54c916
ghpvampiro <gustavo_hp@hotmail.com>
2022-05-02 10:56:41 -0300

    Update sidenav.php

--- src/sidenav.php
+++ src/sidenav.php
@@ -1,9 +1,4 @@
-<!-- 
-  Autor: Gustavo Patricio
-  Data de criação: 01/05/2022
-  Data de modificação: 01/05/2022
-  Versão: 0.01
--->
+
 
 <?php
 
@@ -12,8 +7,6 @@ $sidenav =
 '<div class="sidenav">
 <a href="#">Página Inicial</a>
 <a href="#">API</a>
-<a href="#">Cadastrar Usuário</a>
-<a href="#">Registrar Acesso</a>
 <a href="https://github.com/gushpat">Acessar no GitHub</a>
 
 <b><img src="https://s2.glbimg.com/paF5KTEVGzMU-ZcZa2mjYicNDjM=/e.glbimg.com/og/ed/f/original/2015/03/09/ada.jpg" alt="Ada Lovelace" width="100" ><p>Ada v.1a</p></b>
