26a03e974dd69801825fa5b0fc0f90e7ce8f8c61
ghpvampiro <gustavo_hp@hotmail.com>
2022-05-04 13:43:44 -0300

    Update utils.php

--- src/utils.php
+++ src/utils.php
@@ -28,19 +28,13 @@ function acaoStatus($ativo) //função que recebe um bool e retorna seu valor em
     }
 }
 
-function formatDate($dateTime) //função que recebe uma data no formato YYYY-MM-DD e retorna no formato DD/MM/YYYY
+function formatDate($date) //função que recebe uma data no formato YYYY-MM-DD e retorna no formato DD/MM/YYYY
 {
     //2022-05-04 22:21:32
 
-    $dateTime = explode(" ", $dateTime); //separa a data da hora
-
-    $date = $dateTime[0]; //pega a data
     $date = explode('-', $date);
 
-    $time = $dateTime[1]; //pega a hora
-
-
-    return $date[2].'/'.$date[1].'/'.$date[0].' '.$time; //retorna a data no formato DD/MM/YYYY + hora
+    return $date[2].'/'.$date[1].'/'.$date[0]; //retorna a data no formato DD/MM/YYYY + hora
 
 }
 
